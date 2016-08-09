9dc1b2fcf4797ca4d49b80f0f96eea37dd61312b
surreal6 <surreal@asturnazari.com>
2016-08-09 16:20:42 +0200

    add generated MarkDown

--- pandoc.sh
+++ pandoc.sh
@@ -1,3 +1,5 @@
 pandoc -f markdown_github -t html5 --include-in-header=fonts.js --css pandoc.css -o doc.html 1-Introduction.md 2-Integration.md 3-Communication.md 4-Customization.md
 
 pandoc -f markdown_github -t html5 --include-in-header=fonts.js -s -S --toc --include-in-header=./doc_templates/header_toc.html --css pandoc.css -o README.html 1-Introduction.md 2-Integration.md 3-Communication.md 4-Customization.md
+
+pandoc --from html --to markdown_github -o README.md README.html
