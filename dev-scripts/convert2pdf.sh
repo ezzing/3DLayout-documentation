c5139a526a2118cf71f89d307d08f9c42f02f772
surreal6 <surreal@asturnazari.com>
2016-08-31 17:20:06 +0200

    doc auto-release

--- dev-scripts/convert2pdf.sh
+++ dev-scripts/convert2pdf.sh
@@ -1,6 +1,6 @@
 ~/sources/wkhtmltox/bin/wkhtmltopdf --page-offset 1 --margin-right 25mm --margin-left 25mm --margin-top 25mm --margin-bottom 30mm --header-html ./doc_templates/header.html --header-spacing 5 --footer-html ./doc_templates/footer.html  toc ./doc.html ./3DLayout-doc.pdf
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=./3DLayout-doc-ago.pdf ./doc_templates/portada.pdf ./3DLayout-doc.pdf
-gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=./3DLayout-doc-ago-2016.pdf ./3DLayout-doc-ago.pdf ./doc_templates/contraportada.pdf
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=./3DLayout-doc-sept.pdf ./doc_templates/portada.pdf ./3DLayout-doc.pdf
+gs -dBATCH -dNOPAUSE -q -sDEVICE=pdfwrite -sOutputFile=./3DLayout-doc-sept-2016.pdf ./3DLayout-doc-sept.pdf ./doc_templates/contraportada.pdf
 rm ./3DLayout-doc.pdf
-rm ./3DLayout-doc-ago.pdf
+rm ./3DLayout-doc-sept.pdf
 rm ./doc.html
