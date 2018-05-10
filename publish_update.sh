62890d484b415c3c6a7650013d5e960f01b4c3d1
carlos padial <surreal@asturnazari.com>
2018-05-10 12:42:35 +0200

    separate generate and publish scripts

---
+++ publish_update.sh
@@ -0,0 +1,4 @@
+git add 3DLayout-doc.pdf README.html README.md
+git add 1-Introduction.md 2-Integration.md 3-Communication.md 4-Customization.md 5-changelog.md
+git commit -m "doc auto-release"
+git push origin master
